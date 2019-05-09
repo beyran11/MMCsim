05327f221588d56a77ffeb3e6c55a489672d1619
beyran11 <39955447+beyran11@users.noreply.github.com>
2019-05-09 14:20:48 +0430

    Different D values

--- SSAofMMCbySong/DCchopper.m
+++ SSAofMMCbySong/DCchopper.m
@@ -3,20 +3,26 @@ function DCchopper
 % clc
 % clear all
 % close all
-% 
-% 
-
-D = 0.5 ;
-s = 0 ;
-for n=1:200
-    s = s + (1 - cos(2*pi*n*D))/(n*pi)^2 ;
-    Svec(n) = s ;
+%
+%
+for i=1:5
+    D = 0.1*i ;
+    s = 0 ;
+    for n=1:50
+        s = s + (1 - cos(2*pi*n*D))/(n*pi)^2 ;
+        Svec(n) = s ;
+    end
+    s
+    
+    D*(1-D)
+    
+    plot(1:n,Svec,'k','linewidth',1)
+    hold on
+    plot(1:n,D*(1-D)*ones(1,n),'--b')
 end
-s
 
-D*(1-D)
+xlim([1 50])
+ylim([0.0 0.26])
 
-plot(1:n,Svec)
-hold on
-plot(1:n,D*(1-D)*ones(1,n))
-ylim([0.2 0.26])
+xlabel('Harmonic Order')
+ylabel('Sequence Value')
